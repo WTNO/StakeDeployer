054aee6288cae5334a63764c9bb28ec98d8d70f4
zhongjiaqi <920093991@qq.com>
2023-08-14 18:07:47 +0800

    重新部署好开发环境，调试

--- step/deploy_step.go
+++ step/deploy_step.go
@@ -15,7 +15,7 @@ func Step1() {
 	//command.RunCommand("/bin/bash", "-c", "curl -LO https://github.com/ethereum/staking-deposit-cli/releases/download/v2.5.0/staking_deposit-cli-d7b5304-linux-amd64.tar.gz")
 
 	// 解压
-	//command.RunCommand("/bin/bash", "-c", "tar xvf staking_deposit-cli-d7b5304-linux-amd64.tar.gz")
+	command.RunCommand("/bin/bash", "-c", "tar xvf staking_deposit-cli-d7b5304-linux-amd64.tar.gz")
 
 	// 运行存款工具
 	e, _, err := expect.Spawn("./staking_deposit-cli-d7b5304-linux-amd64/deposit new-mnemonic --num_validators 2 --chain goerli --eth1_withdrawal_address 0x4D496CcC28058B1D74B7a19541663E21154f9c84", -1)
