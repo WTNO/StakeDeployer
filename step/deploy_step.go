351145af8ee75914933c79081788accdd129499e
zhongjiaqi <920093991@qq.com>
2023-08-15 21:27:54 +0800

    调试

--- step/deploy_step.go
+++ step/deploy_step.go
@@ -5,6 +5,7 @@ import (
 	expect "github.com/google/goexpect"
 	"github.com/wtno/StakeDeployer/command"
 	"github.com/wtno/StakeDeployer/file"
+	"os"
 	"regexp"
 	"strings"
 	"time"
@@ -19,7 +20,7 @@ func Step1() {
 	//command.RunCommand("/bin/bash", "-c", "tar xvf staking_deposit-cli-d7b5304-linux-amd64.tar.gz")
 
 	// 运行存款工具
-	e, _, err := expect.Spawn("./staking_deposit-cli-d7b5304-linux-amd64/deposit new-mnemonic --num_validators 2 --chain goerli --eth1_withdrawal_address 0x4D496CcC28058B1D74B7a19541663E21154f9c84", -1)
+	e, _, err := expect.Spawn("./staking_deposit-cli-d7b5304-linux-amd64/deposit new-mnemonic --num_validators 2 --chain goerli --eth1_withdrawal_address 0x4D496CcC28058B1D74B7a19541663E21154f9c84", -1, expect.Verbose(true), expect.VerboseWriter(os.Stdout))
 	fmt.Println("Spawn : " + e.String())
 	if err != nil {
 		fmt.Println(err)
