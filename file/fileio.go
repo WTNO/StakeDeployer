cebce5811bde26919d92ddcb3e87d938eba5858d
zhongjiaqi <920093991@qq.com>
2023-08-16 19:49:20 +0800

    步骤8 配置执行客户端Geth 以及 重写了ReadAndWriteFile方法

--- file/fileio.go
+++ file/fileio.go
@@ -3,24 +3,29 @@ package file
 import (
 	"fmt"
 	"io"
-	"log"
 	"os"
 )
 
 func ReadAndWriteFile(inputFilePath, outputFilePath string) error {
+	file, err := os.Create(outputFilePath)
+	if err != nil {
+		fmt.Println("创建文件失败：", err)
+		return err
+	}
+
 	// 读取文件内容
 	content, err := os.ReadFile(inputFilePath)
 	if err != nil {
 		return err
 	}
 
-	// 写入文件
-	err = os.WriteFile(outputFilePath, content, 0644)
+	_, err = io.WriteString(file, string(content))
 	if err != nil {
+		fmt.Println("写入文件失败：", err)
 		return err
 	}
 
-	log.Printf("write to %s success!", outputFilePath)
+	fmt.Println("文件写入成功")
 	return nil
 }
 
