2725147d982867bd044cf8674426bfeb639cf2ef
zhongjiaqi <920093991@qq.com>
2023-08-21 18:59:00 +0800

    调试

--- step/deploy_step.go
+++ step/deploy_step.go
@@ -323,6 +323,9 @@ func Step13() {
 	command.RunExpect(e, ".*Enter the password for your.*", "12345678\n")
 	command.RunExpect(e, ".*Importing accounts.*", "")
 
+	fmt.Println("wait for 10 seconds ...")
+	time.Sleep(10 * time.Second)
+
 	// 创建钱包密码文件
 	file.CreateAndWriteFile("cptbtptp", "/var/lib/prysm/validator/password.txt")
 
